08220b20513ea74ddcffe23a16e7b9a2abf23245
Matt Hale <mlhale@unomaha.edu>
2023-03-01 13:04:24 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,30 @@
+input.onButtonPressed(Button.A, function () {
+    password = "" + passwordinput + "A"
+})
+// reset password and door when both buttons are pressed
+input.onButtonPressed(Button.AB, function () {
+    passwordinput = ""
+    if (pins.analogReadPin(AnalogPin.P0) != 180) {
+        // close the door
+        pins.servoWritePin(AnalogPin.P0, 180)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    password = "" + passwordinput + "B"
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    if (password == passwordinput) {
+        basic.showIcon(IconNames.Yes)
+        // Open the door
+        pins.servoWritePin(AnalogPin.P0, 90)
+    } else {
+        basic.showIcon(IconNames.No)
+        passwordinput = ""
+    }
+})
+let passwordinput = ""
+let password = ""
+// password composed of a's and b's (also a great band)
+password = "ABBA"
+// close the door
+pins.servoWritePin(AnalogPin.P0, 180)
